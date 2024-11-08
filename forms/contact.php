4c3f5c0348545e347cec36ac97337a06a70d9c08
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-11-08 15:13:25 -0300

    últimas alterações antes de hospedar

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,6 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+ 
+  $receiving_email_address = 'contato@mobicuritiba.com.br';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -26,7 +19,7 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $contact->smtp = array(
-    'host' => 'example.com',
+    'host' => 'mobicuritiba.com.br',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
